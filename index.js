da175058e83b41e7f178ff0bcc05637a06e35f47
Patrick A Kim <pkim050@gmail.com>
2018-12-22 17:10:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -148,6 +148,8 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
   if (e.which === LEFT_ARROW) {
+    e.preventDefault();
+    e.stopPropagation();
     moveDodgerLeft();
   }
   else if (e.which === RIGHT_ARROW) {
